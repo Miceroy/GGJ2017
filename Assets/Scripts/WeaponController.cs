863a9eb667db780e7b658dfa9cc1434648125685
Mikko Romppainen <kajakbros@gmail.com>
2017-01-20 21:13:52 +0200

    Text mode prefabs.

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
+
 public class WeaponController : MonoBehaviour
 {
     public GameObject hitParticle;

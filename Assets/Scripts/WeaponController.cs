b63cdfa622e687e34e4aaa7848d3032fbad35ace
Mikko Romppainen <kajakbros@gmail.com>
2017-01-20 19:11:41 +0200

    up

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class WeaponController : MonoBehaviour {
+    public float m_cooldownTime = 1.0f;
+
+    bool m_canShoot;
     bool m_shoot;
     bool m_melee;
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        enableShooting();
+    }
+
+    void enableShooting()
+    {
+        m_canShoot = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         m_shoot = CrossPlatformInputManager.GetButtonDown("Fire1");
         m_melee = CrossPlatformInputManager.GetButtonDown("Fire2");
+        if (m_canShoot && m_shoot)
+        {
+            m_canShoot = false;
+            Invoke("enableShooting", m_cooldownTime);
+            Debug.Log("Shoot = " + m_shoot.ToString() );
+        }
+        else if (m_canShoot && m_melee)
+        {
+            m_canShoot = false;
+            Invoke("enableShooting", m_cooldownTime);
+            Debug.Log("Melee = " + m_melee.ToString());
+        }
+        else
+        {
+        }
 
-        Debug.Log("Shoot = " + m_shoot.ToString() + " Melee = " + m_melee.ToString());
     }
 }

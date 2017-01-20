f3e035c2707cc091ad0864f8e3ae75af9364ed7f
Joona Tiinanen <joona_tiinanen@hotmail.com>
2017-01-20 20:48:33 +0200

    First enemies

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private GameObject m_playerObject;
+
+	// Use this for initialization
+	void Start ()
+    {
+        m_playerObject = GameObject.Find("FPSController");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 enemyToPlayer = (m_playerObject.transform.position - transform.position);
+
+        transform.Translate(enemyToPlayer.normalized * Time.deltaTime * 8f);
+
+        if (enemyToPlayer.magnitude < 5f)
+            Destroy(gameObject);
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    // Props
+    [SerializeField] private float m_MinimumInterval = 1f;
+    [SerializeField] private float m_MaximumInterval = 5f;
+
+    private float m_timer = 0f;
+    private float m_nextSpawnAt = 0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (m_nextSpawnAt <= (m_timer += Time.deltaTime))
+        {
+            m_nextSpawnAt = Random.Range(m_MinimumInterval, m_MaximumInterval);
+            m_timer = 0f;
+
+            // Spawn enemy
+            GameObject enemy = (GameObject)Instantiate(Resources.Load("Prefabs/EnemyCharacter", typeof(GameObject)), transform.position, transform.rotation);
+        }
+	}
+}

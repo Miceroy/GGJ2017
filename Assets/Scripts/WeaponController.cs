ab1623321a4e930570347dca251e2f4dff6a9c4f
Mikko Romppainen <kajakbros@gmail.com>
2017-01-20 19:03:31 +0200

    Added WeaponController.

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class WeaponController : MonoBehaviour {
+    bool m_shoot;
+    bool m_melee;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        m_shoot = CrossPlatformInputManager.GetButtonDown("Fire1");
+        m_melee = CrossPlatformInputManager.GetButtonDown("Fire2");
+
+        Debug.Log("Shoot = " + m_shoot.ToString() + " Melee = " + m_melee.ToString());
+    }
+}

3b21fcea9edb3f0ab416ec5c6e7e243fb9f06ab4
Mikko Romppainen <kajakbros@gmail.com>
2017-01-21 12:01:02 +0200

    Added HealthBarController.

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -4,13 +4,32 @@ using UnityEngine;
 
 public class HealthController : MonoBehaviour {
     public float m_health = 100;
+
+    private float m_maxHealth;
+
+    void Start()
+    {
+        m_maxHealth = m_health;
+    }
     
     public void applyDamage(float damage)
     {
         m_health -= damage;
+        
         if (m_health <= 0.0f)
         {
+            m_health = 0.0f;
             Invoke("deleteMe", 0.0f);
+        }
+
+        if (gameObject.tag == "Player")
+        {
+            GameObject hb = GameObject.FindGameObjectWithTag("HealthBar");
+            if(hb != null)
+            {
+                HealthBarController hbc = hb.GetComponent<HealthBarController>();
+                hbc.setValue(m_health, m_maxHealth);
+            }
         }
     }
 
